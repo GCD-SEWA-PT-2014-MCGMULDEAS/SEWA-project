50442ba5ec53a7957e2eb26c41ca8c7f941614fd
Cotch1985 <cathal.mulkere@gmail.com>
2015-01-03 20:26:58 +0000

    Fixing index css on About Us pagse

--- Final Project Submission/about.php
+++ Final Project Submission/about.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <title>GFC STATS.COM</title>
-  <link rel = "stylesheet" type="text/css" href="index2.css" />
+  <link rel = "stylesheet" type="text/css" href="index.css" />
 </head>
 
 
