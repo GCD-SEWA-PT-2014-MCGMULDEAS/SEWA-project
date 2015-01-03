5370e35611e77157be9b1388ab2a4fb24a6459eb
gavinmcg <g.mcgovern84@gmail.com>
2015-01-03 18:11:46 +0000

    Validating index

--- Final Project Submission/Index.php
+++ Final Project Submission/Index.php
@@ -22,7 +22,7 @@
 
 	<!-- Script to rotate images on the site -->
   
-  <script language="JavaScript1.2">
+  <script type="text/JavaScript1.2">
 
 	var howOften = 5; //number often in seconds to rotate
 	var current = 0; //start the counter at 0
@@ -132,7 +132,7 @@ Gavs css ? Section for login and logo in header
 	
 	</div>
 </nav>
-<!--Gavs code using buttons**********************************************************
+Gavs code using buttons**********************************************************
                <input type = "button" name = "home" value = "Home" onclick = "window.location.href='./index.php'">
                <input type = "button" name = "compare" value = "Comparison" onclick = "window.location.href='./comparison.php'">
                <input type = "button" name = "about" value = "About Us" onclick = "window.location.href='./about.php'">
@@ -146,11 +146,11 @@ NavBar is the section to the left with the links to the text-->
 <div id = "navBar">
 
 	
-		<p> <h3> <a href = "#comp">Be Competitive</a></h3></p><br>
+		<h3> <a href = "#comp">Be Competitive</a></h3><br>
 <!-- ****************ANCHOR LINK HERE TO GO TO LINE ********************-->		
-		<p> <h3><a href="#analyse">Analyse yourself</a></h3></p><br>
-		<p> <h3><a href="#fact">Some Facts</a></h3></p><br>
-		<p> <h3><a href="#comp">Read More</a></h3></p><br>
+		<h3><a href="#analyse">Analyse Yourself</a></h3><br>
+		<h3><a href="#fact">Some Facts</a></h3><br>
+		<h3><a href="#comp">Read More</a></h3><br>
 </div>
 
 <!-- **************************************************************************************
@@ -167,7 +167,7 @@ TwitBar takes in the twitter feed -->
 
 <!-- ***********************************************************************************
 placeholder is the middle section for the rotating images--> 
-<layer id="placeholderlayer"></layer>
+<!--<layer id="placeholderlayer"></layer>-->
 <div id="placeholderdiv"></div>
 
 <!--************************************************************************************ 
@@ -177,20 +177,20 @@ Section with the body text for the main page
 <div class="page-wrap" id="page-wrap">
   
  <h1 id = "comp">Be Competitive</h1>
- <p class = 'right'><img src="largeImages/jump.jpg" height="10%">
- Sport has always been competitive and as teams and individuals look to find that competitive edge optimal use of time and technology can make sure you are the best that you can be. Training alone can be challenging but setting goals and knowing the targets your competitors are aiming for and achieving can help focus the mind and body.</br>
+ <p class = 'right'><img src="largeImages/jump.jpg" alt="Jump">
+ Sport has always been competitive and as teams and individuals look to find that competitive edge optimal use of time and technology can make sure you are the best that you can be. Training alone can be challenging but setting goals and knowing the targets your competitors are aiming for and achieving can help focus the mind and body.<br/>
 
-</br>
+<br/>
 
  GFC Stats is all about you. Much of the information available on the site is public knowledge, though not always easy to find. In recent years a lot of effort and investment has gone in to analysing what works and what doesn’t. Although the results don’t produce a revolutionary short cut to success, what they do is produce a template for a quicker path to success. Thus template enables you to focus on those areas of your game that need improving to achieve even better results.</p>
 
-  <h1 id= "analyse" style = "text-align:center">Analyse yourself</style></h1>
-  <p class="left"><img style="float:left" src="largeImages/compare.jpg" height="10%"> 
+  <h1 id= "analyse" style = "text-align:center">Analyse yourself</h1>
+  <p class="left"><img style="float:left" src="largeImages/compare.jpg" alt="compare"> 
   Each week you can build up a record of the training you have done, the goals you have achieved and the targets you want to pass. Your input can be compared with those of your peers, those of the elite players and against averages compiled  over time. By including your match facts you will be able to not only compare yourself to these other players but you can determine the relevant improvements in performance over time and how these translate to key performance indicators in matches.</p>
  
   <p class="right"> These show areas where improvement is quite achievable with the correct training. The key areas of focus are Strength, Speed/Agility, Fitness and Flexibility when it comes to the non- skills element of training. These are all areas that you can work on in your own time alone or with friends. Keeping of records of what is achieved is key to determining the effectiveness and future direction of your training. </p>
  
- </br>
+ <br/>
 
   <h1 id="fact">Facts to challenge you</h1>
   
@@ -214,9 +214,9 @@ Footer details here
 <div class="col">
   <h2>Sample links</h2>
   <ul>
-    <li><a href="#">Terms & Conditions</a></li><br/>
-    <li><a href="#">Sitemap</a></li><br/>
-    <li><a href="#">Useful links</a></li><br/>
+    <li><a href="#">Terms & Conditions</a></li>
+    <li><a href="#">Sitemap</a></li>
+    <li><a href="#">Useful links</a></li>
   </ul>
 </div>
 <div class="col">
@@ -230,10 +230,10 @@ Footer details here
 <div class="col" id = "image">
   <h2>You can find us on:</h2>
   <ul>
-    <li><a href="#facebook"><img style="float:left" src="largeImages/facebook.png" width="50px" height="50px"></a></li>
-    <li><a href="#linkedin"><img style="float:left" src="largeImages/linkedin.jpg" width="50px" height= "50px"></a></li>
-    <li><a href="#instagram"><img style="float:left" src="largeImages/instagram.png" width="50px" height= "50px"></a></li>
-    <li><a href="#twitter"><img style="float:left" src="largeImages/twitter.png" width="50px" height= "50px"></a></li>
+    <li><a href="#facebook"><img style="float:left" src="largeImages/facebook.png" width="50" height="50" alt="facebook"></a></li>
+    <li><a href="#linkedin"><img style="float:left" src="largeImages/linkedin.jpg" width="50" height= "50" alt="linkdin"></a></li>
+    <li><a href="#instagram"><img style="float:left" src="largeImages/instagram.png" width="50" height= "50" alt="instagram"></a></li>
+    <li><a href="#twitter"><img style="float:left" src="largeImages/twitter.png" width="50" height= "50" alt="twitter"></a></li>
   </ul>
 </div>
 </footer>
