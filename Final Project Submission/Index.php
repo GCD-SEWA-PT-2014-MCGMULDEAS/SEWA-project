1697ffd34d7f23c0087905ee14fd05b4f34680bf
gavinmcg <g.mcgovern84@gmail.com>
2015-01-03 15:24:42 +0000

    Fixing a link

--- Final Project Submission/Index.php
+++ Final Project Submission/Index.php
@@ -86,7 +86,7 @@ Gavs css ? Section for login and logo in header
                 </ul>
               </li>
               <li>
-                 <a href="about.html">About us <span class="arrow">&#9660;</span></a>
+                 <a href="about.php">About us <span class="arrow">&#9660;</span></a>
    
                 <ul class="sub-menu">
                     <li><a href="about.php#who">Who we are</a></li>
