324772f021c2be89ba24ea980ec0404c00148278
Cotch1985 <cathal.mulkere@gmail.com>
2015-01-03 23:45:05 +0000

    Login.php button fix

--- Final Project Submission/login.php
+++ Final Project Submission/login.php
@@ -16,11 +16,11 @@
 
 <div id = "content">
 	<form name = "Login" method = "POST" action = "validate.php">
-		<h1>Welcome. Please Login.</h1>
+		<h1>Welcome, Please Login:</h1>
 		<ul>
 			<li><label>Username:</label></li><li><input type = "text" name = "username" id = "username"/></li>
 			<li><label>Password:</label></li><li><input type= "password" name="password" id = "password"/></li>
-			<li><input type = "submit" name = "submit" value = "login"/><input type = "button" name = "Register" value = "Register" onclick = "window.location.href='./register.php'"></li>
+			<li><input type = "submit" name = "submit" value = "Login"/><input type = "button" name = "Register" value = "Register" onclick = "window.location.href='./register.php'"></li>
 
 		</ul>
 
