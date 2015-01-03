3b2c5a2b74094ce00abd2da72b573794a6756dd7
Cotch1985 <cathal.mulkere@gmail.com>
2015-01-03 13:43:25 +0000

    Contact Page and CSS Update

--- contact.php
+++ contact.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 
+<?php 
+	$key = "AIzaSyDOd7zDjy5MtmLJei0AORAXUGLsuwW6iXk";
+	/*For Server = AIzaSyD3H-YxeHsZHOVa2EfiePuJOwjGgrRSpOk*/
+	//also set up one final variable where we concatenate these vairables in addition to code for the embedded sateliite view of the google maps API.
+	$embeddedCode = "&q=" . "Griffith College Dublin" . "," . "Dublin" . "," . "Ireland" . "&zoom=20&maptype=satellite";
+?>
+
 <html>
 
 <head>
@@ -23,21 +30,28 @@
 </script>
 
 <title>Contact Us</title>
+  <meta charset="UTF-8">
+ <link rel="stylesheet" type="text/css" href="contactus.css"/>
 
 </head>
 
 <body>
 
-	<?php include ("header.php");?>
+<div class = "header">	<?php include ("header.php");?></div>
 
+<div class = "forminline">
 <form name = "email" method = "POST" action = "sendmail.php" onSubmit = "return validateForm();">
-Name<br \><input type = "textbox" name = "name" value = "name"><br \>
-Email<br \><input type = "textbox" name = "email" value = "email"><br \>
-Subject<br \><input type = "textbox" name = "subject" value = "subject"><br \>
-Message<br \><textarea name = "message" rows = "4" cols = "50"></textarea><br \>
-<input type = "submit" name = "submit" value = "Submit Form"/>
+Name<br \><input type = "textbox" name = "name"><br/>
+Email<br \><input type = "textbox" name = "email"><br/>
+Subject<br \><input type = "textbox" name = "subject"><br/>
+Message<br \><textarea name = "message" rows = "10" cols = "50"></textarea><br/>
+<input type = "submit" name = "submit" value = "Submit Form"/><br/><br/>
+
+<iframe frameborder="2" marginheight="2" marginwidth="2"
+src= "https://www.google.com/maps/embed/v1/place?key=<?php echo $key; ?><?php echo $embeddedCode; ?>"></iframe>
 
 </form>
+</div>
 
 </body>
 
