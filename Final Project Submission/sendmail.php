c2cec7d3c8f55b1d14431486a0727d023acf9a6f
Cotch1985 <cathal.mulkere@gmail.com>
2015-01-04 13:39:42 +0000

    Mistake
    sendmail update - will keep existing version

--- Final Project Submission/sendmail.php
+++ Final Project Submission/sendmail.php
@@ -5,14 +5,17 @@ if($_POST["name"]==""||$_POST["email"]==""||$_POST["subject"]==""||$_POST["messa
 echo "Fill All Fields..";
 }else{
 
+$altemail = "gaelicgamesstatistics@gmail.com";
 $email=$_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $headers = 'From:'. $email . "\r\n"; // Sender's Email
+/*$headers = 'From:'. $email . "\r\n"; //my email*/
 // Message lines should not exceed 70 characters (PHP rule), so wrap it
 $message = wordwrap($message, 70);
 // Send Mail By PHP Mail Function
-mail("gfcstats@gfc.dxhost.net", $subject, $message, $headers);
+/*"gfcstats@gfc.dxhost.net"*/
+mail($altemail, $subject, $message, $headers);
 echo "Your mail has been sent successfuly ! Thank you for your feedback";
 
 }
