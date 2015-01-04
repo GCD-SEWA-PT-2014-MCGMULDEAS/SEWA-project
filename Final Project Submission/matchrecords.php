9f985590f82df7e7d0954bb27307bcd5f43ef7cf
gavinmcg <g.mcgovern84@gmail.com>
2015-01-04 01:41:14 +0000

    Match record validation + some updates to record styles css

--- Final Project Submission/matchrecords.php
+++ Final Project Submission/matchrecords.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 include './dbconnection.php';
 
@@ -77,8 +78,8 @@ if($RowMA1[0] == null){
 	echo "<h1>You have not entered any Match Records!</h1>";
 } else {
 echo "<h1> Your Match records are as follows: </h1>";
-echo "<table width='80%' border='1'>";
-echo "<tr><th colspan='10' align='center'>Match Records Previously Entered</th></tr>";
+echo "<table border='1'>";
+echo "<tr><th colspan='10'>Match Records Previously Entered</th></tr>";
 echo "<tr><td>Date</td><td>Match Type</td><td>Minutes Played</td><td>Distance Covered (mtrs)</td><td>No. of Possesions</td><td>Passes Complete</td><td>Passes Incomplete</td><td>Attempts</td><td>Goals</td><td>Points</td></tr>";
 do {
 echo "<tr><td>{$RowMA1[1]}</td>";
@@ -93,7 +94,7 @@ echo "<td>{$RowMA1[17]}</td>";
 echo "<td>{$RowMA1[18]}</td></tr>";
 $RowMA1 = mysqli_fetch_row($outputMatch);
 } while ($RowMA1);  
-
+echo "</table>";
 };
 
 ?>
